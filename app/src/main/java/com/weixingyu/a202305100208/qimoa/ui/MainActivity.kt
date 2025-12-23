0c06e21746c0adb62f63891aa6f60cb7301546ee
weixingyu <1474119072@qq.com>
2025-12-23 20:03:28 +0800

    主界面框架搭建

--- app/src/main/java/com/weixingyu/a202305100208/qimoa/ui/MainActivity.kt
+++ app/src/main/java/com/weixingyu/a202305100208/qimoa/ui/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.weixingyu.a202305100208.qimoa.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.weixingyu.a202305100208.qimoa.R
+import com.weixingyu.a202305100208.qimoa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 1. 一进来默认显示 HomeFragment
+        replaceFragment(HomeFragment())
+
+        // 2. 监听底部按钮点击
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_chart -> {
+                    replaceFragment(ChartFragment())
+                    true
+                }
+                R.id.navigation_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
         }
+    }
+
+    // 切换 Fragment 的通用方法
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
